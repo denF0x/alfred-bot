95f6090e20370d1a0a3169df2855e92693a25f38
Denis <fox-94.09@yandex.ru>
2020-04-09 18:44:02 +0300

    photo bot method

--- src/main/java/AlfredBot.java
+++ src/main/java/AlfredBot.java
@@ -1,40 +1,83 @@
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 public class AlfredBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
-
-        if (update.hasMessage() && update.getMessage().hasText()) {
+        //for all for now
+        if (update.hasMessage()) {
             String user_first_name = update.getMessage().getChat().getFirstName();
             String user_last_name = update.getMessage().getChat().getLastName();
             String user_username = update.getMessage().getChat().getUserName();
             long user_id = update.getMessage().getChat().getId();
-            String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-            String answer = "Нет, " + user_first_name + " это ты - " + message_text;
 
-            SendMessage message = new SendMessage()
-                    .setChatId(chat_id)
-                    .setText(answer);
+            //if we got text
+            if (update.getMessage().hasText()) {
+                String message_text = update.getMessage().getText();
+                String answer = "Нет, " + user_first_name + " это ты - " + message_text;
+
+                SendMessage message = new SendMessage()
+                        .setChatId(chat_id)
+                        .setText(answer);
 
 
-            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+                log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            //if we got photo
+            //TODO: lesson 2
+            else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+                List<PhotoSize> photos = update.getMessage().getPhoto();
+                // Know file_id
+                String f_id = photos.stream()
+                            .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                            .findFirst()
+                            .orElse(null).getFileId();
+                // Know photo width
+                int f_width = photos.stream()
+                        .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                        .findFirst()
+                        .orElse(null).getWidth();
+                // Know photo height
+                int f_height = photos.stream()
+                        .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                        .findFirst()
+                        .orElse(null).getHeight();
+
+                String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
+
+                SendPhoto msg = new SendPhoto()
+                                    .setChatId(chat_id)
+                                    .setPhoto(f_id)
+                                    .setCaption(caption);
+
+                try{
+                    execute(msg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
     }
-
 
 
     private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
@@ -48,7 +91,7 @@ public class AlfredBot extends TelegramLongPollingBot {
 
 
     public String getBotUsername() {
-      return System.getenv("userName");
+        return System.getenv("userName");
 
     }
 
