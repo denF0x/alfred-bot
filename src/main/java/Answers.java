8f99db8c924cba80ed49e62baae0b1062709a8d5
Denis <fox-94.09@yandex.ru>
2020-04-14 22:11:01 +0300

    test rhyme

--- src/main/java/Answers.java
+++ src/main/java/Answers.java
@@ -1,32 +1,34 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Answers {
 
-    private static ArrayList <String> morningAnswers = new ArrayList<>();
-    private static List<String> eveningAnswers = new ArrayList<>();
+     private static ArrayList <String> morningAnswers = new ArrayList<>();
+     private static List<String> eveningAnswers = new ArrayList<>();
 
-    Answers(){
-        addToMorningList("А кто это у нас тут проснулься? Ты проснулься!");
-        addToMorningList("Очередное карантинное утро. Не правда ли замечательно?");
-        addToMorningList("Мороз и солнце.\\n День чудесный.\\n Еще не сдохли мы?\\n Прелестно");
-        addToMorningList("");
-        addToMorningList("");
-        addToMorningList("");
-        addToMorningList("");
-        addToMorningList("");
-        addToMorningList("");
-        addToMorningList("");
-    }
+
+
 
 
     public static String getMorningAnswer() {
+        fillMorningList();
         try {
             return morningAnswers.get(2);
             //return morningAnswers.get((int) (Math.random() * 10));
         } catch (Exception e) {
             return "Хуютро";
         }
+    }
+
+    private static void fillMorningList() {
+
+
+        addToMorningList("А кто это у нас тут проснулься? Ты проснулься!");
+        addToMorningList("Очередное карантинное утро. Не правда ли замечательно?");
+        addToMorningList("Мороз и солнце.\\n День чудесный.\\n Еще не сдохли мы?\\n Прелестно");
+
     }
 
     public static String getEveningAnswer() {
@@ -37,7 +39,7 @@ public class Answers {
         }
     }
 
-    public void addToMorningList(String text){
-        this.morningAnswers.add(text);
+    public static void addToMorningList(String text){
+        morningAnswers.add(text);
     };
 }
