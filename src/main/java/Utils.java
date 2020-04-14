23c199bf490774eb51e441d6c0f01c7b5462dc46
Denis <fox-94.09@yandex.ru>
2020-04-15 01:43:35 +0300

    security

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,7 +1,9 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
 public class Utils {
-    public static String getRandomStringFromListSizeOfList(ArrayList<String> list){
+    public static String getRandomStringFromListSizeOfList(@NotNull ArrayList<String> list){
         return list.get((int) (Math.random() * list.size()));
     }
 }
